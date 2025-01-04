cb8d2fbb7d348d105916a53547466367ac66acc8
manvip28 <159559667+manvip28@users.noreply.github.com>
2025-01-04 23:52:40 +0530

    Delete lib directory

--- lib/widgets/widgets.dart
+++
@@ -1,6 +0,0 @@
-// lib/widgets/widgets.dart
-export 'patient_tabs/patient_overview_tab.dart';
-export 'patient_tabs/patient_vitals_tab.dart';
-export 'patient_tabs/patient_records_tab.dart';
-export 'vitals_monitor_widget.dart';
-export 'health_trends_chart.dart';
